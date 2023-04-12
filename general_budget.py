ffb34002418c2c7b9a4c718c281ce469989e7111
Mohamed Faris <mohd.faris07@gmail.com>
2023-04-12 17:52:38 +0300

    More refactor

--- general_budget.py
+++ general_budget.py
@@ -1,138 +1,87 @@
-from sys import argv
 import re
 import csv
-
-date = []
-creditcard = 0.0
-fuel = 0.0
-rent = 0.0
-car = 0.0
-coffee = 0.0
-cash = 0.0
-bills = 0.0
-deliveries = 0.0
-groceries = 0.0
-eout = 0.0
-savings = 0.0
-income = 0.0
-cleaner = 0.0
-other = 0.0
-ikea = 0.0
-susie = 0.0
-
-# patterns
-groceries_pattern = re.compile(
-    "HYPERMARKET|SUPERMARKET|LHM|FOOD WO|ALJAZIRA|MIDWAY|CARREFOUR|ALNOOR"
-)
-eat_out_pattern = re.compile(
-    "Talabat|STARBUCKS|CARIBOU|YOUNIS|YASALAM|SHAKE SHACK|URBAN SLICE|FIVE GUYS"
-)
-deliveries_pattern = re.compile("ARAMEX|Aramex|aramex")
-bills_pattern = re.compile("Batelco|stc")
-cash_pattern = re.compile("WDL")
-coffee_pattern = re.compile("CRUST")
-cleaner_pattern = re.compile("BH25NBOB00000276923162")
-rent_pattern = re.compile("200000983477")
-car_pattern = re.compile("200005196913")
-savings_pattern = re.compile("200006043230|200004304353")
-fuel_pattern = re.compile("7896941550852504000")
-fuel_pattern_2 = re.compile("ila")
-ikea_pattern = re.compile("IKEA")
-creditcard_pattern = re.compile("CrediMax")
+from sys import argv
 
 
-# date function to get the date
-# TODO specify dates to get calculated expenditure
-def get_date():
+class TransactionCategorizer:
+    def __init__(self):
+        self.categories = {
+            "groceries": (
+                "HYPERMARKET|SUPERMARKET|LHM|FOOD WO|ALJAZIRA|MIDWAY|CARREFOUR|ALNOOR",
+                0.0,
+            ),
+            "eat_out": (
+                "Talabat|STARBUCKS|CARIBOU|YOUNIS|YASALAM|SHAKE SHACK|URBAN SLICE|FIVE GUYS",
+                0.0,
+            ),
+            "deliveries": ("ARAMEX|Aramex|aramex", 0.0),
+            "bills": ("Batelco|stc", 0.0),
+            "cash": ("WDL", 0.0),
+            "coffee": ("CRUST", 0.0),
+            "cleaner": ("BH25NBOB00000276923162", 0.0),
+            "rent": ("200000983477", 0.0),
+            "car": ("200005196913", 0.0),
+            "savings": ("200006043230|200004304353", 0.0),
+            "fuel": ("7896941550852504000", 0.0),
+            "fuel_2": ("ila", 0.0),
+            "ikea": ("IKEA", 0.0),
+            "creditcard": ("CrediMax", 0.0),
+            "income": ("", 0.0),
+            "other": ("", 0.0),
+            "susie": ("", 0.0),
+        }
+
+    def update_category(self, category_name, amount):
+        current_amount = self.categories[category_name][1]
+        self.categories[category_name] = (
+            self.categories[category_name][0],
+            current_amount + amount,
+        )
+
+    def categorize_transaction(self, row):
+        for category, (pattern, amount) in self.categories.items():
+            if re.search(pattern, row[1]) is not None:
+                if (
+                    category == "income"
+                    and re.search("([0-9]*\.[0-9]{3})$", row[4]) is not None
+                ):
+                    match = re.search("([0-9]*\.[0-9]{3})$", row[4])
+                    transaction = float(match.group(0))
+                    self.update_category(category, transaction)
+                elif category != "income":
+                    match = re.search("([0-9]*\.[0-9]{3})(\-)$", row[4])
+                    transaction = float(match.group(2) + match.group(1))
+                    self.update_category(category, transaction)
+                break
+        else:
+            match = re.search("([0-9]*\.[0-9]{3})(\-)$", row[4])
+            transaction = float(match.group(2) + match.group(1))
+            self.update_category("other", transaction)
+
+
+def get_date(row):
+    date = []
     match = re.search("^FDR.*([A-Z]{3})([0-9]{2})", row[1])
     if match:
         day = match.group(2) + "/" + match.group(1) + "/2020"
         date.append(day)
     else:
         date.append(row[0])
+    return date
 
 
-# function to calculate transaction amount
-def trans_calc(var):
-    match = re.search("([0-9]*\.[0-9]{3})(\-)$", row[4])
-    transaction = float(match.group(2) + match.group(1))
-    var = var + transaction
-    return var
-
-
-def categorise():
-    global creditcard
-    global fuel
-    global ikea
-    global rent
-    global car
-    global cleaner
-    global coffee
-    global cash
-    global bills
-    global deliveries
-    global income
-    global groceries
-    global eout
-    global savings
-    global susie
-    global other
-    if re.search(groceries_pattern, row[1]) is not None:
-        groceries = trans_calc(groceries)
-    elif re.search(eat_out_pattern, row[1]) is not None:
-        eout = trans_calc(eout)
-    elif re.search("([0-9]*\.[0-9]{3})$", row[4]) is not None:
-        match = re.search("([0-9]*\.[0-9]{3})$", row[4])
-        transaction = float(match.group(0))
-        income += transaction
-    elif re.search(deliveries_pattern, row[1]) is not None:
-        deliveries = trans_calc(deliveries)
-    elif re.search(bills_pattern, row[1]) is not None:
-        bills = trans_calc(bills)
-    elif re.search(cash_pattern, row[1]) is not None:
-        cash = trans_calc(cash)
-    elif re.search(coffee_pattern, row[1]) is not None:
-        coffee = trans_calc(coffee)
-    elif re.search(cleaner_pattern, row[1]) is not None:
-        cleaner = trans_calc(cleaner)
-    elif re.search(rent_pattern, row[1]) is not None:
-        rent = trans_calc(rent)
-    elif re.search(car_pattern, row[1]) is not None:
-        car = trans_calc(car)
-    elif re.search(savings_pattern, row[1]) is not None:
-        savings = trans_calc(savings)
-    elif re.search(fuel_pattern, row[1]) is not None:
-        fuel = trans_calc(fuel)
-    elif re.search(fuel_pattern_2, row[1]) and re.match("10|15", row[4]) is not None:
-        fuel = trans_calc(fuel)
-    elif re.search(ikea_pattern, row[1]) is not None:
-        ikea = trans_calc(ikea)
-    elif re.search(creditcard_pattern, row[1]) is not None:
-        creditcard = trans_calc(creditcard)
-    else:
-        other = trans_calc(other)
-
-
-with open(argv[1], newline="") as csvfile:
-    reader = csv.reader(csvfile)
+def main():
+    transaction_categorizer = TransactionCategorizer()
+    dates = []
+    with open(argv[1], newline="") as csvfile:
+        reader = csv.reader(csvfile)
     for row in reader:
-        # print(row[0])
-        # date_match = re.search("^FDR", row[1])
-        get_date()
-        categorise()
+        dates.append(get_date(row)[0])
+        transaction_categorizer.categorize_transaction(row)
 
-print("Income: %.3f" % income)
-print("Savings: %.3f" % savings)
-print("Groceries: %.3f" % groceries)
-print("Eating out: %.3f" % eout)
-print("Susie: %.3f" % susie)
-print("Coffee: %.3f" % coffee)
-print("Car payment: %.3f" % car)
-print("Bills: %.3f" % bills)
-print("Credit card: %.3f" % creditcard)
-print("Fuel: %.3f" % fuel)
-print("Deliveries from Aramex: %.3f" % deliveries)
-print("Cash withdrawals: %.3f" % cash)
-print("Cleaner: %.3f" % cleaner)
-print("IKEA: %.3f" % ikea)
-print("Other? = %.3f" % other)
+    for category, (_, amount) in transaction_categorizer.categories.items():
+        print(f"{category.capitalize()}: {amount:.3f}")
+
+
+if __name__ == "__main__":
+    main()
